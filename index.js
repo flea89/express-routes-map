de4a5c099fcc4f9b33e4adb93e9fe3cee5ab72ac
Paolo Chillari <killer.paolo@gmail.com>
2013-03-22 23:55:36 +0100

    Added the chance to define a middleware by route resource

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
-module.exports = function (app){
-	var i=0;
+module.exports = function(app) {
+	var i = 0;
 	var routes = {};
 	routes.array = [];
-	routes.defineRoutes = function(route, obj){
-		this.array.push( {
-			routeName : route,
-			routeObj: obj
+	routes.defineRoutes = function(route, obj, middleware) {
+		this.array.push({
+			routeName: route,
+			routeObj: obj,
+			middleware: middleware
 		});
-		return this;	
+		return this;
 	};
-	routes.map = function () {
-		var collection ,
-			item,
-			routeName;
-		this.array.forEach( function(el,index){
-
+	routes.map = function() {
+		var collection,
+		item,
+		routeName,
+		middleware;	
+		this.array.forEach(function(el, index) {
+			var key;
 			collection = el.routeObj.collection;
 			item = el.routeObj.item;
 			routeName = el.routeName;
-
-			for ( var key in collection){
-				if (collection.hasOwnProperty(key)) {
-					app[key](routeName, collection[key]);
+			middleware = el.middleware;
+			if (typeof middleware === 'function') {
+				for (key in collection) {
+					if (collection.hasOwnProperty(key)) {
+						app[key](routeName, middleware, collection[key]);
+					}
+				}
+				for (key in item) {
+					if (item.hasOwnProperty(key)) {
+						app[key](routeName + '/:item',middleware, item[key]);
+					}
 				}
 			}
-			for (key in item){
-				if (item.hasOwnProperty(key)) {
-					app[key](routeName + '/:item', item[key]);
+			else {
+				for (key in collection) {
+					if (collection.hasOwnProperty(key)) {
+						app[key](routeName, collection[key]);
+					}
 				}
-			}	
+				for (key in item) {
+					if (item.hasOwnProperty(key)) {
+						app[key](routeName + '/:item', item[key]);
+					}
+				}
+
+			}
 		});
 	};
 
